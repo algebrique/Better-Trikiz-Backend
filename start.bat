6727bffd1abb619ab4d01e128ccb767c2a94538a
Poly <114899640+2flpp@users.noreply.github.com>
2025-04-21 02:01:03 +0200

    caca
    caca

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+title Better Trikiz Backend
+
+:start
+node index.js
+goto start
---
+++ structs/log.js
@@ -0,0 +1,123 @@
+const fs = require("fs");
+const config = JSON.parse(fs.readFileSync("./Config/config.json").toString());
+
+function getTimestamp() {
+    const now = new Date();
+    const date = now.toLocaleDateString('en-US');
+    const time = now.toLocaleTimeString();
+    
+    return `${date} ${time}`; 
+}
+
+function formatLog(prefixColor, prefix, ...args) {
+    let msg = args.join(" ");
+    let formattedMessage = `${prefixColor}[${getTimestamp()}] ${prefix}\x1b[0m: ${msg}`;
+    console.log(formattedMessage);
+}
+
+function backend(...args) {
+    let msg = args.join(" ");
+    if (config.bEnableFormattedLogs) {
+        formatLog("\x1b[32m", "Better Trikiz Backend Log", ...args);
+    } else {
+        console.log(`\x1b[32mBetter Trikiz Backend Log\x1b[0m: ${msg}`);
+    }
+}
+
+function bot(...args) {
+    let msg = args.join(" ");
+    if (config.bEnableFormattedLogs) {
+        formatLog("\x1b[33m", "Better Trikiz Bot Log", ...args);
+    } else {
+        console.log(`\x1b[33mBetter Trikiz Bot Log\x1b[0m: ${msg}`);
+    }
+}
+
+function xmpp(...args) {
+    let msg = args.join(" ");
+    if (config.bEnableFormattedLogs) {
+        formatLog("\x1b[34m", "Better Trikiz Xmpp Log", ...args);
+    } else {
+        console.log(`\x1b[34mBetter Trikiz Xmpp Log\x1b[0m: ${msg}`);
+    }
+}
+
+function error(...args) {
+    let msg = args.join(" ");
+    if (config.bEnableFormattedLogs) {
+        formatLog("\x1b[31m", "Better Trikiz Error Log", ...args);
+    } else {
+        console.log(`\x1b[31mBetter Trikiz Error Log\x1b[0m: ${msg}`);
+    }
+}
+
+function debug(...args) {
+    if (config.bEnableDebugLogs) {
+        let msg = args.join(" ");
+        if (config.bEnableFormattedLogs) {
+            formatLog("\x1b[35m", "Better Trikiz Debug Log", ...args);
+        } else {
+            console.log(`\x1b[35mBetter Trikiz Debug Log\x1b[0m: ${msg}`);
+        }
+    }
+}
+
+function website(...args) {
+    let msg = args.join(" ");
+    if (config.bEnableFormattedLogs) {
+        formatLog("\x1b[36m", "Better Trikiz Website Log", ...args);
+    } else {
+        console.log(`\x1b[36mBetter Trikiz Website Log\x1b[0m: ${msg}`);
+    }
+}
+
+function AutoRotation(...args) {
+    if (config.bEnableAutoRotateDebugLogs) {
+        let msg = args.join(" ");
+        if (config.bEnableFormattedLogs) {
+            formatLog("\x1b[36m", "Better Trikiz AutoRotation Debug Log", ...args);
+        } else {
+            console.log(`\x1b[36mBetter Trikiz AutoRotation Debug Log\x1b[0m: ${msg}`);
+        }
+    }
+}
+
+function checkforupdate(...args) {
+    let msg = args.join(" ");
+    if (config.bEnableFormattedLogs) {
+        formatLog("\x1b[33m", "Better Trikiz Update Log", ...args);
+    } else {
+        console.log(`\x1b[33mBetter Trikiz Update Log\x1b[0m: ${msg}`);
+    }
+}
+
+function autobackendrestart(...args) {
+    let msg = args.join(" ");
+    if (config.bEnableFormattedLogs) {
+        formatLog("\x1b[92m", "Better Trikiz Auto Backend Restart Log", ...args);
+    } else {
+        console.log(`\x1b[92mBetter Trikiz Auto Backend Restart\x1b[0m: ${msg}`);
+    }
+}
+
+function calderaservice(...args) {
+    let msg = args.join(" ");
+    if (config.bEnableFormattedLogs) {
+        formatLog("\x1b[91m", "Caldera Service Log", ...args);
+    } else {
+        console.log(`\x1b[91mCaldera Service\x1b[0m: ${msg}`);
+    }
+}
+
+module.exports = {
+    backend,
+    bot,
+    xmpp,
+    error,
+    debug,
+    website,
+    AutoRotation,
+    checkforupdate,
+    autobackendrestart,
+    calderaservice
+};
